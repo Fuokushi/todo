bd44ab016405b0b704e4f81112b5d7a4b152e37b
lev.yarysh <lioness.yarush@gmail.com>
2026-02-25 08:32:07 +0200

    2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 const mongoose = require('mongoose')
-const mongoDB = mongoDB = "mongodb+srv://lionessyarush:Gfhjkm18022009@cluster0.othdvv9.mongodb.net/todos?appName=Cluster0"
+const mongoDB = "mongodb+srv://lionessyarush:Gfhjkm18022009@cluster0.othdvv9.mongodb.net/todos?appName=Cluster0"
 mongoose.connect(mongoDB)
 
 
